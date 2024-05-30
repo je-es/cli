b751ad173abf199d7fbe7b5f27353f193621a71d
Maysara <Maysara.Elshewehy@gmail.com>
2024-05-30 07:32:43 +0300

    0.0.5

--- src/code/main.ts
+++ src/code/main.ts
@@ -13,37 +13,5 @@
 /* ---------------------------------------- PACK ----------------------------------------  */
 
     export *                                from './modules/cli';
-    import { cli, t_cli }                                from './modules/cli';
 
-    const myCli : t_cli =
-      {
-          info:
-          {
-              name            : 'mecs',
-              description     : 'A simple CLI application',
-              version         : '1.0.0'
-          },
-
-          actions:
-          {
-              create:
-              {
-                  flag       : '-c',
-                  alias       : '--create',
-                  args        : ['name'],
-                  required    : ['as'],
-                  callback    : ({ name, type }) =>
-                  {
-                      console.log(`Creating project '${name} as ${type}'.`);
-                  }
-              },
-
-              as:
-              {
-                  args        : ['type']
-              }
-          }
-      };
-
-      cli(myCli);
 /* ---------------------------------------- ---- ----------------------------------------  */
